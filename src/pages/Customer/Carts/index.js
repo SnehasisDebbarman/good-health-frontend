471627634f6e5ee3da79d3b95134a5bfe8607c94
snehasis <snehasisdebbarman2016@gmail.com>
2023-03-16 20:01:18 +0530

    ,..

--- src/pages/Customer/Carts/index.js
+++ src/pages/Customer/Carts/index.js
@@ -22,29 +22,33 @@ export default function Index() {
 
   useEffect(() => {
     router.beforePopState(async ({ as }) => {
-      if (as !== router.asPath) {
-        //
-        const fakeData = {
-          username: faker.name.firstName(),
-          email: userEmail,
-          phone: faker.phone.number("+91##########"),
-          abandonedStatus: !OrderPlaced,
-          url: "http://localhost:3001/Customer/Carts",
-          others: others,
-        };
-        const response = await fetch(
-          "https://goode-health-server.onrender.com/abandonedCart",
-          // "http://localhost:3002/abandonedCart",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(fakeData),
-          }
-        );
-        console.log("user leave the cart", (await response.json()) ?? {});
+      if (OrderPlaced) {
+      } else {
+        if (as !== router.asPath) {
+          //
+          const fakeData = {
+            username: faker.name.firstName(),
+            email: userEmail,
+            phone: faker.phone.number("+91##########"),
+            abandonedStatus: !OrderPlaced,
+            url: "http://localhost:3001/Customer/Carts",
+            others: others,
+          };
+          const response = await fetch(
+            "https://goode-health-server.onrender.com/abandonedCart",
+            // "http://localhost:3002/abandonedCart",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(fakeData),
+            }
+          );
+          console.log("user leave the cart", (await response.json()) ?? {});
+        }
       }
+
       return true;
     });
 
@@ -53,14 +57,14 @@ export default function Index() {
     };
   }, []);
 
-  const [hydrated, setHydrated] = React.useState(false);
-  React.useEffect(() => {
-    setHydrated(true);
-  }, []);
-  if (!hydrated) {
-    // Returns null on first render, so the client and server match
-    return null;
-  }
+  // const [hydrated, setHydrated] = React.useState(false);
+  // React.useEffect(() => {
+  //   setHydrated(true);
+  // }, []);
+  // if (!hydrated) {
+  //   // Returns null on first render, so the client and server match
+  //   return null;
+  // }
   return (
     <div className="min-h-screen w-full bg-white text-black p-8 grid grid-cols-2">
       <div className="">
@@ -126,11 +130,66 @@ export default function Index() {
             setOrderPlaced(true);
             alert("Order Placed");
             setCarts([]);
+            router.push("/Customer");
           }}
           className="mt-3 w-full text-lg bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
         >
           Place order
         </button>
+
+        <div
+          id="defaultModal"
+          tabindex="-1"
+          aria-hidden="true"
+          class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
+        >
+          <div class="relative w-full h-full max-w-2xl md:h-auto">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                  Order Placed
+                </h3>
+                <button
+                  type="button"
+                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="defaultModal"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button
+                  data-modal-hide="defaultModal"
+                  type="button"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  I accept
+                </button>
+                <button
+                  data-modal-hide="defaultModal"
+                  type="button"
+                  class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                >
+                  Decline
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
